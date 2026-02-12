7ee2ceb41ab72675286fc6123fcdf60cc18f6a6d
Malyshenko <echelxn.rage@yandex.ru>
2026-02-13 00:24:04 +0300

    some quality changes + slider hover pause

--- js/slider.js
+++ js/slider.js
@@ -7,8 +7,12 @@ function moveSlider(i) {
     index = i;
     track.style.transform = `translateX(-${index * 100}%)`;
 
-    dots.forEach(dot => dot.classList.remove('hero__dot--active'));
-    dots[index].classList.add('hero__dot--active');    
+    dots.forEach(dot => {
+        dot.classList.remove('hero__dot--active');
+        dot.removeAttribute("aria-current");
+    });
+    dots[index].classList.add('hero__dot--active');   
+    dots[index].setAttribute("aria-current", true); 
 }
 
 dots.forEach((dot, i) => {
@@ -17,8 +21,10 @@ dots.forEach((dot, i) => {
     })
 });
 
-function restartAutoplay() {
-    let autoplay = setInterval(() => {
+let autoplay;
+
+function startAutoplay() {
+    autoplay = setInterval(() => {
         index++;
 
         if (index >= dots.length) {
@@ -27,11 +33,16 @@ function restartAutoplay() {
         
         moveSlider(index);
 
-    }, 1000);
+    }, 5000);
+    
 }
 
-restartAutoplay();
+startAutoplay();
+
+function pauseAutoplay() {
+    clearInterval(autoplay);
+}
 
 
-// track.addEventListener('mouseenter', () => clearInterval(autoplay));
-// track.addEventListener('mouseleave', () => restartAutoplay());
+track.addEventListener('mouseenter', () => pauseAutoplay());
+track.addEventListener('mouseleave', () => startAutoplay());
