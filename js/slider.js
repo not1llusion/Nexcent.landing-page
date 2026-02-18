100f7f34133922bb2efa0ffc74c44b50bff069bd
Malyshenko <echelxn.rage@yandex.ru>
2026-02-19 00:00:50 +0300

    some adaptive + burger button need to make a lot of remakes, but need to go forward

--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,21 @@
 const track = document.querySelector(".hero__track");
 const dots = document.querySelectorAll(".hero__dot");
+const slider = document.querySelector(".hero__slider");
 let index = 0;
 
 
 function moveSlider(i) {
     index = i;
-    track.style.transform = `translateX(-${index * 100}%)`;
+
+    const w = slider.clientWidth;
+    track.style.transform = `translateX(-${index * w}px)`;
 
     dots.forEach(dot => {
         dot.classList.remove('hero__dot--active');
         dot.removeAttribute("aria-current");
     });
     dots[index].classList.add('hero__dot--active');   
-    dots[index].setAttribute("aria-current", true); 
+    dots[index].setAttribute("aria-current", "true"); 
 }
 
 dots.forEach((dot, i) => {
@@ -24,16 +27,7 @@ dots.forEach((dot, i) => {
 let autoplay;
 
 function startAutoplay() {
-    autoplay = setInterval(() => {
-        index++;
-
-        if (index >= dots.length) {
-            index = 0;
-        }
-        
-        moveSlider(index);
-
-    }, 5000);
+     autoplay = setInterval(() => moveSlider((index + 1) % dots.length), 5000);
     
 }
 
@@ -46,3 +40,19 @@ function pauseAutoplay() {
 
 track.addEventListener('mouseenter', () => pauseAutoplay());
 track.addEventListener('mouseleave', () => startAutoplay());
+
+
+const openBurger = document.querySelector(".btn--burger");
+const closeBurger = document.querySelector(".btn-close");
+
+const burgerMenu = document.querySelector(".mobile-menu");
+
+openBurger.addEventListener("click", () => {
+    burgerMenu.classList.add("is-open");
+    console.log("opened")
+})
+
+closeBurger.addEventListener("click", () => {
+    burgerMenu.classList.remove("is-open");
+    console.log("closed")
+});
